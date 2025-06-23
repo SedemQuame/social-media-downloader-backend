91374c0068b8d7019912e6059f430b64cad5cbca
sedem <sedem.amekpewu.3@gmail.com>
2025-06-23 00:13:47 +0000

    minor change

--- src/app.js
+++ src/app.js
@@ -1,22 +1,45 @@
 require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
 const apiRoutes = require("./routes/api");
 
 const app = express();
 const NODE_ENV = process.env.NODE_ENV || "development";
 const PORT = process.env.PORT || 3000;
+const baseUrl = `http://localhost:${PORT}`;
 
+app.use(
+    cors({
+        origin: "*",
+        methods: ["GET", "POST", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    })
+);
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
+app.all(`/`, (req, res) => {
+    res.status(200).json({ status: "ok" });
+});
+app.use((req, res, next) => {
+    if (
+        req.header("x-forwarded-proto") !== "https" &&
+        process.env.NODE_ENV === "production"
+    ) {
+        res.redirect(`https://${req.header("host")}${req.url}`);
+    } else {
+        next();
+    }
+});
 app.use(express.json());
-app.get("/", (req, res) => {
-    res.status(200).json({ status: "ok" });
-});
-app.post("/", (req, res) => {
-    res.status(200).json({ status: "ok" });
-});
 app.use("/api", apiRoutes);
 app.listen(PORT, () => {
-    const baseUrl = `http://localhost:${PORT}`;
     switch (NODE_ENV) {
         case "production":
             console.log(`App running in PRODUCTION at ${baseUrl}`);
